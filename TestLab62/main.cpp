996f82603e5030839e2a61c60204b4c125e0815a
Pivinter <70973362+Pivinter@users.noreply.github.com>
2022-10-21 11:28:45 +0300

    Add project files.

---
+++ TestLab62/main.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+using namespace std;
+void Create(int* a, const int size, const int Low, const int High)
+{
+	for (int i = 0; i < size; i++)
+		a[i] = Low + rand() % (High - Low + 1);
+}
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << "a[" << setw(2) << i << " ] = " << setw(4) << a[i] << endl;
+	cout << endl;
+}
+int Max(int* a, const int size)
+{
+	int max = a[0];
+	for (int i = 1; i < size; i++)
+		if (a[i] > max)
+			max = a[i];
+	return max;
+}
+int Min(int* a, const int size)
+{
+	int min = a[0];
+	for (int i = 1; i < size; i++)
+		if (a[i] < min)
+			min = a[i];
+	return min;
+}
+int main()
+{
+	srand((unsigned)time(NULL)); // ініціалізація генератора випадкових чисел
+	const int n = 25;
+	int a[n];
+	int Low = 5;
+	int High = 90;
+	Create(a, n, Low, High);
+	Print(a, n);
+
+	cout << "max = " << Max(a, n) << endl;
+	cout << "min = " << Min(a, n) << endl;
+
+	return 0;
+}
---
+++ UnitTest62u/UnitTest62u.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62u
+{
+	TEST_CLASS(UnitTest62u)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
